1bb5698ce0504e0e00c1c814850ebbd20d8a5fdf
Branko Daskijević <branko.dask@gmail.com>
2022-06-16 21:50:36 +0200

    adding selectors for form data

--- website/script.js
+++ website/script.js
@@ -1,5 +1,19 @@
-/* nav */
+// nav
 const nav = document.querySelector(".nav")
+
+// form data
+const submitButton = document.getElementById("contact-submit")
+const form = document.getElementById("contact")
+const guestName = document.getElementById("guestName").value
+const guestEmail = document.getElementById("guestEmail").value
+const guestPhoneNumber = document.getElementById("guestPhoneNumber").value
+const guestDescription = document.getElementById("guestDescription").value
+const guestHouse1 = document.getElementById("teddy").value
+const guestHouse2 = document.getElementById("lili").value
+const guestNumber = document.getElementById("guestNumber").value
+const guestArrivalDate = document.getElementById("guestArrival").value
+const guestLeaveDate = document.getElementById("guestLeave").value
+
 window.addEventListener("scroll", fixNav)
 
 function fixNav() {
@@ -25,3 +39,47 @@ function removeActiveClasses() {
 		panel.classList.remove("active")
 	})
 }
+
+// submitButton.addEventListener("submit", (event) => {
+// 	event.preventDefault()
+
+// 	const data = {
+// 		guestName,
+// 		guestEmail,
+// 		guestPhoneNumber,
+// 		guestDescription,
+// 		guestHouse1,
+// 		guestHouse2,
+// 		guestNumber,
+// 		guestArrivalDate,
+// 		guestLeaveDate
+// 	}
+
+// 	console.log(data)
+// })
+
+// submitButton.addEventListener("submit", (event) => {
+// 	event.preventDefault()
+
+// 	console.log("submited")
+// })
+
+function formHandler(e) {
+	e.preventDefault()
+
+	const data = {
+				guestName,
+				guestEmail,
+				guestPhoneNumber,
+				guestDescription,
+				guestHouse1,
+				guestHouse2,
+				guestNumber,
+				guestArrivalDate,
+				guestLeaveDate
+			}
+
+	console.log(data)
+}
+
+form.addEventListener("submit", formHandler)
